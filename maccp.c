a358316c4c49ef544bf9865e9b2017975efe60c2
Raag Jadav <raag.jadav@intel.com>
2023-10-01 04:28:04 +0530

    maccp: ring buffer support
    Signed-off-by: Raag Jadav <raag.jadav@intel.com>

--- maccp.c
+++ maccp.c
@@ -8,16 +8,19 @@
 
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
+#include <poll.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <net/if.h>
 #include <netinet/ether.h>
 #include <unistd.h>
 
+#define ETH_P_DEAD	0xdead
 #define MTU		1500
 
 FILE *fp;
@@ -28,6 +31,9 @@ int sockfd;
 struct ether_header *eh;
 struct ether_addr *destmac;
 struct sockaddr_ll sockaddr;
+
+struct iovec *ring;
+struct tpacket_req req;
 
 int open_file(char *path, char *mode)
 {
@@ -125,6 +131,7 @@ int prep_send(char *iface)
 
 int prep_recv(char *iface)
 {
+	int i, ret;
 #if 0
 	struct ifreq ifopts;
 
@@ -135,21 +142,53 @@ int prep_recv(char *iface)
 	ioctl(sockfd, SIOCSIFFLAGS, &ifopts);
 #endif
 
+	/* Setup socket for ring buffer */
+	req.tp_block_size = 4096;
+	req.tp_frame_size = sizeof(struct ether_header) + MTU + 534;	// 534 padding
+	req.tp_block_nr = 64;
+	req.tp_frame_nr = req.tp_block_nr * (req.tp_block_size / req.tp_frame_size);
+	ret = setsockopt(sockfd, SOL_PACKET, PACKET_RX_RING, (void *)&req, sizeof(struct tpacket_req));
+	if (ret < 0) {
+		perror("PACKET_RX_RING");
+		goto out;
+	};
+
+	/* Map it */
+	buf = mmap(NULL, req.tp_block_size * req.tp_block_nr, PROT_READ | PROT_WRITE, MAP_SHARED, sockfd, 0);
+	if (buf == MAP_FAILED) {
+		perror("mmap");
+		ret = -1;
+		goto out;
+	}
+
+	/* Setup ring buffer */
+	ring = malloc(req.tp_frame_nr * sizeof(struct iovec));
+	if (!ring) {
+		perror("malloc");
+		ret = -1;
+		goto out_unmap;
+	}
+
+	for (i = 0; i < req.tp_frame_nr; i++) {
+		ring[i].iov_base = (void *)((long)buf) + (i * req.tp_frame_size);
+		ring[i].iov_len = req.tp_frame_size;
+	}
+
 	/* Bind socket to the network device */
-	if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, iface, IFNAMSIZ-1) < 0) {
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, iface, IFNAMSIZ-1);
+	if (ret < 0) {
 		perror("SO_BINDTODEVICE");
-		return -1;
+		goto out_ring;
 	}
-
-	buf = calloc(1, sizeof(struct ether_header) + MTU);
-	if (!buf) {
-		perror("calloc");
-		return -1;
-	}
-
-	eh = (struct ether_header *)buf;
 
 	return 0;
+
+out_ring:
+	free(ring);
+out_unmap:
+	munmap(buf, req.tp_block_size * req.tp_block_nr);
+out:
+	return ret;
 }
 
 int start_send()
@@ -172,7 +211,7 @@ int start_send()
 		}
 
 		/* Ethertype/size field */
-		eh->ether_type = htons(data_size);
+		eh->ether_type = htons(ETH_P_DEAD);
 		tx_len += data_size;
 
 		/* Send packet */
@@ -190,9 +229,10 @@ int start_send()
 int start_recv(char *iface)
 {
 	size_t eh_len = sizeof(struct ether_header);
-	size_t buf_size = eh_len + MTU;
+	size_t frame_size = eh_len + MTU;
 	struct ifreq if_mac;
-	ssize_t numbytes;
+	struct pollfd pfd;
+	int i;
 
 	/* Get the MAC address of the interface */
 	memset(&if_mac, 0, sizeof(struct ifreq));
@@ -202,28 +242,44 @@ int start_recv(char *iface)
 		return -1;
 	}
 
-repeat:
-	memset(buf, 0, buf_size);
-	numbytes = recvfrom(sockfd, buf, buf_size, 0, NULL, NULL);
-
-	/* Check if the packet is for me */
-	if (eh->ether_dhost[0] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[0] &&
-			eh->ether_dhost[1] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[1] &&
-			eh->ether_dhost[2] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[2] &&
-			eh->ether_dhost[3] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[3] &&
-			eh->ether_dhost[4] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[4] &&
-			eh->ether_dhost[5] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[5]) {
-
-		if (fwrite(buf + eh_len, numbytes - eh_len, 1, fp) != 1) {
-			perror("fwrite");
-			return -1;
-		}
-
-		if (numbytes < buf_size)
-			return 0;
+	/* Sleep when nothing's happening */
+	pfd.fd = sockfd;
+	pfd.events = POLLIN | POLLERR;
+	pfd.revents = 0;
+	if (poll(&pfd, 1, -1) < 0) {
+		perror("poll");
+		return -1;
 	}
 
-	goto repeat;
+	for (i = 0;;) {
+		while(*(unsigned long *)ring[i].iov_base & TP_STATUS_USER) {
+			struct tpacket_hdr *th = ring[i].iov_base;
+			char *bp = (char *)th + th->tp_mac;
+			eh = (struct ether_header *)bp;
+
+			/* Check if the packet is for me */
+			if (eh->ether_dhost[0] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[0] &&
+					eh->ether_dhost[1] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[1] &&
+					eh->ether_dhost[2] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[2] &&
+					eh->ether_dhost[3] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[3] &&
+					eh->ether_dhost[4] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[4] &&
+					eh->ether_dhost[5] == ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[5]) {
+
+				if (fwrite(bp + eh_len, th->tp_len - eh_len, 1, fp) != 1) {
+					perror("fwrite");
+					return -1;
+				}
+
+				/* Tell the kernel this packet is done with */
+				th->tp_status = TP_STATUS_KERNEL;
+
+				if (th->tp_len < frame_size)
+					return 0;
+
+				i = (i == req.tp_frame_nr - 1) ? 0 : i + 1;
+			}
+		}
+	}
 }
 
 int maccp_send(char *iface, char *path)
@@ -261,7 +317,7 @@ int maccp_recv(char *iface, char *path)
 	if (ret)
 		goto out;
 
-	ret = open_sock(htons(ETH_P_ALL));
+	ret = open_sock(htons(ETH_P_DEAD));
 	if (ret)
 		goto out_file;
 
@@ -271,7 +327,8 @@ int maccp_recv(char *iface, char *path)
 
 	ret = start_recv(iface);
 
-	free(buf);
+	free(ring);
+	munmap(buf, req.tp_block_size * req.tp_block_nr);
 out_sock:
 	close(sockfd);
 out_file:
